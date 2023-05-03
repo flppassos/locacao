5d4935cdfd278f31215d92ba739984cb8be380f3
Felipe <felipepassos08@gmail.com>
2023-05-03 19:45:57 -0300

    Criado a view inicial

---
+++ resources/views/inicial.blade.php
@@ -0,0 +1,7 @@
+<x-admin.layouts.principal>
+    {{-- Dentro a gente passa o conteúdo que quer --}}
+    {{-- Vai aparecer no lugar do $slot --}}
+
+    <h1>Inicial</h1>
+    
+</x-admin.layouts.principal>
