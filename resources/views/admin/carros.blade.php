14f9fa7563b51738c8d58bff35c73fb88e4ca265
Felipe Passos <felipepassos08@gmail.com>
2023-05-29 12:32:37 -0300

    Ajustes layout

---
+++ resources/views/admin/carros.blade.php
@@ -0,0 +1,7 @@
+<x-admin.layouts.principal>
+
+    <h1>Veículos</h1>
+
+    <x-admin.carros />
+
+</x-admin.layouts.principal>
---
+++ resources/views/components/admin/carros.blade.php
@@ -0,0 +1,3 @@
+<div class="bg-amber-500">
+    Componente Veículos
+</div>
